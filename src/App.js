b9337696a1f619d92bfde3f5d0e67c3e8a48c02c
kadjiah <kadijahwilson@pursuit.org>
2021-04-13 17:38:59 -0400

    keep track of rating in state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import EmptyStar from './assets/empty-star.svg';
 import FilledStar from './assets/filled-star.svg';
 import HalfStar from './assets/half-star.svg';
 import './styles.css';
 
 
-const RatingSystem = (amount) => {
-  const stars = new Array(amount.amount)
+const RatingSystem = (props) => {
+  const [rating, setRating] = useState(0)
+
+  const stars = new Array(props.amount)
   stars.fill(1, 0)
-  // console.log(stars)
+  // console.log(props)
+  function handleRating(i){
+    setRating(i + 1)
+  }
+
   return (
     <div>
       <h1>5 star rating system</h1>
       <h2>Select a rating:</h2>
         <div className='bar'>
           {stars.map((star, i) => 
-            <div className='star'>
+            <div className='star' onClick={() => {handleRating(i)}}>
               <img src={EmptyStar} alt="empty star" />
             </div>
           )}
