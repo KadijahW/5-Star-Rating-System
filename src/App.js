95b6dae609ee24c70ce59ed9e445465d2a0f5c0e
kadjiah <kadijahwilson@pursuit.org>
2021-05-04 17:34:42 -0400

    5 star rating

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import EmptyStar from './assets/empty-star.svg';
 import FilledStar from './assets/filled-star.svg';
 // import HalfStar from './assets/half-star.svg';
 import './styles.css';
-
+//git remote set-url origin (url)
 
 const RatingSystem = (props) => {
   const [rating, setRating] = useState(0)
   const [hover, setHover] = useState(0)
+  const [clicks, setClicks] = useState(0) //clicks
+  const [total, setTotal]  = useState(0)//avg
+
+  const userRating = new Array(props.amount)
+  userRating.fill(1,0)
 
   const stars = new Array(props.amount)
   stars.fill(1, 0)
   // console.log(props)
 
   function handleRating(i){
-    if(rating === i + 1) setRating(0)
-    else setRating(i + 1)
+    if(rating === i + 1){
+      setRating(0)
+    } 
+    else {
+      setRating(i + 1)
+      setClicks(clicks + 1)
+      setTotal((total + i + 1 ))
+    }
   }
 
   function handleHover(i){
@@ -26,6 +37,7 @@ const RatingSystem = (props) => {
     <div>
       <h1>5 star rating system</h1>
       <h2>Select a rating:</h2>
+      <h1>User Rating</h1>
         <div className='bar'>
           {stars.map((star, i) => 
             <div 
@@ -36,13 +48,23 @@ const RatingSystem = (props) => {
               onMouseOut={()=> {setHover(0)}}
             >
               <img 
-                src = {rating > i || hover > i ? FilledStar : EmptyStar} 
+                src =  {rating > i || hover > i ? FilledStar : EmptyStar} 
                 alt = {rating > i || hover > i ? "Filled-Star" : "Empty-Star"} 
               />
             </div>
           )}
+            </div>
+            <h1>Average:{total/clicks}</h1>
+            <div className='userBar'>
+              {userRating.map((userRating, i)  => 
+                <div className='userRate'>
+              <img src={total/clicks >= i + 1 ? FilledStar : EmptyStar} />
+            </div>
+          )}
+          </div>
+          
         </div>
-    </div>
+    // </div>
   );
 }
 
